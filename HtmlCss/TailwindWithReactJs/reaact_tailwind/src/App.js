325aee132d5339cd51f4b2ca16887db8b5226f14
Sudarshan <sudarshan@metamindsystem.com>
2023-12-02 21:28:22 +0530

    Tailwind

---
+++ HtmlCss/TailwindWithReactJs/reaact_tailwind/src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">Tailwind Starting</header>
+      <section>
+        <button className="border bg-black">aksjjdfnm</button>
+      </section>
+    </div>
+  );
+}
+
+export default App;
